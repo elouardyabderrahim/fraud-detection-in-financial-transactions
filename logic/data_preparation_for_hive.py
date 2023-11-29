aaf37cee4558d1d80394a2e506cbc3d65db56f37
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-11-29 10:53:01 +0100

    feat #3 read data from api function

--- logic/data_preparation_for_hive.py
+++ logic/data_preparation_for_hive.py
@@ -1,4 +1,10 @@
 from pyhive import hive
+import requests
+
+
+customer_url="http://127.0.0.1:5000/api/customers"
+transactions_url="http://127.0.0.1:5000/api/transactions"
+external_data_url="http://127.0.0.1:5000/api/external"
 
 
 def test_hive_connection():
@@ -24,3 +30,31 @@ if test_hive_connection():
     print("Performing actions on Hive...")
 else:
     print("Connection to Hive failed. Unable to perform actions.")
+
+
+
+def get_data_from_api(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for bad responses
+
+        # Check if the response is in JSON format
+        if 'application/json' in response.headers.get('content-type', '').lower():
+            data = response.json()
+            print("data:", data)
+
+        else:
+            data = response.text
+            print("data:", data)
+
+
+        return data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from API: {e}")
+        return None
+
+
+get_data_from_api(customer_url)
+get_data_from_api(transactions_url)
+get_data_from_api(external_data_url)

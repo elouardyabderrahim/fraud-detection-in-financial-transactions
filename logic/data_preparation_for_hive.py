394d24b726a4493e596c6ea1db5e6450f43fde5c
elouardyabderrahim <elouardy.abderrahim07@gmail.com>
2023-12-01 12:06:17 +0100

    feat: #3 insert transaction data

--- logic/data_preparation_for_hive.py
+++ logic/data_preparation_for_hive.py
@@ -1,6 +1,6 @@
 from pyhive import hive
 import requests
-
+from datetime import datetime as dt
 
 customer_url="http://127.0.0.1:5000/api/customers"
 transactions_url="http://127.0.0.1:5000/api/transactions"
@@ -8,20 +8,18 @@ external_data_url="http://127.0.0.1:5000/api/external"
 
 connect=hive.Connection(host='localhost', port=10000)
 cursor = connect.cursor()
+
+
 def test_hive_connection():
     try:
-        
         cursor.execute("SELECT 1")
         result = cursor.fetchall()
         print("Connection to Hive successful")
-    
-
-
-        return True;
+        return True
     except Exception as e:
-        print(f'Error connecting to Hive{e}')
+        print(f'Error connecting to Hive: {e}')
         return False
-    
+
 # create data base for Fraud detection
 def create_db(db_name):
     try:
@@ -33,36 +31,19 @@ def create_db(db_name):
     return True
 
 
-
-if test_hive_connection():
-    print("Performing actions on Hive...")
-    create_db("frauddb")
-    cursor.execute("show DATABASES ")
-    dbs = cursor.fetchall()
-    for i in dbs:
-        print(i)
-  
-
-else:
-    print("Connection to Hive failed. Unable to perform actions.")
-   
-
-
-
-
 def get_data_from_api(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise an error for bad responses
-
+        response.raise_for_status()  
+        
         # Check if the response is in JSON format
         if 'application/json' in response.headers.get('content-type', '').lower():
             data = response.json()
-            print("data:", data)
+            
 
         else:
             data = response.text
-            print("data:", data)
+    
 
 
         return data
@@ -72,10 +53,54 @@ def get_data_from_api(url):
         return None
 
 
-# get_data_from_api(customer_url)
-# get_data_from_api(transactions_url)
-# get_data_from_api(external_data_url)
 
+
+# create table for transactions
+def create_transactions_table():
+    try:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS frauddb.transactionsssss (
+                transaction_id STRING,
+                date_time  STRING,
+                amount DOUBLE,
+                currency STRING,
+                merchant_details STRING,
+                customer_id STRING,
+                transaction_type STRING,
+                location STRING
+            )
+        """)
+        print("Table 'transaction' created successfully!")
+    except Exception as e:
+        print(f'Error creating table: {e}')
+
+def insert_transactions_data(data):
+    try:       
+        # print("entry",modified_entry)
+         print(data)
+         cursor.execute(f"""
+               INSERT INTO frauddb.transactionsssss
+               VALUES ('{data["transaction_id"]}', '{data["date_time"]}', '{data["amount"]}','{data["currency"]}', '{data["merchant_details"]}','{data["customer_id"]}', '{data["transaction_type"]}', '{data["location"]}')
+            """)
+         
+         print("Transactions data inserted successfully!")
+    except Exception as e:
+        print(f'Error inserting transactions data: {e}')
+
+if test_hive_connection():
+    print("Performing actions on Hive...")
+    create_db("frauddb")
+    cursor.execute("USE frauddb")
+    create_transactions_table()
+
+    transactions_data = get_data_from_api(transactions_url)
+    
+    for data in transactions_data:
+        insert_transactions_data(data)
+
+else:
+    print("Connection to Hive failed. Unable to perform actions.")
 
 cursor.close()
 connect.close()
+
